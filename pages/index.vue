8f212e4cb9119ea9e26284a06da5aca18d9cdd3c
유준호 <jh.ryu@piolink.com>
2020-11-23 18:51:47 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex md:justify-between sm:justify-start bg-indigo-700">
+    <div class="item">test</div>
+    <div class="item">test</div>
+    <div class="item">test</div>
+    <div class="item">test</div>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style>
+
+</style>
